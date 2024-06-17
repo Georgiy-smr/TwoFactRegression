43318f23a07feca0b487fa40af597687c045b7f1
Georgy <mr.smirnovgd@yandex.ru>
2024-06-17 21:24:04 +0500

    Проверка на реальных данных

--- TwoFactRegressCalc/TwoFactRegressCalc/ViewModels/MainViewModel.cs
+++ TwoFactRegressCalc/TwoFactRegressCalc/ViewModels/MainViewModel.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using OfficeOpenXml.FormulaParsing.Excel.Functions;
 using Regression.Two_factor_regression;
 using TwoFactRegressCalc.Infrastructure.Commands.Base;
 using TwoFactRegressCalc.Infrastructure.DI.Services.FileDialog;
@@ -60,9 +61,85 @@ namespace TwoFactRegressCalc.ViewModels
                    return;
                 var d = data.CreateThirdOrderPolynomialExpression();
                 var s = _regression.CalcCoefs(d);
+                var results = data.Select(x1x2y => CalcRes(x1x2y, s.ToArray())).ToList();
+                //var result = CalcRes(data.First(), s.ToArray());
             }
         }
 
+        private double CalcRes(DataTwoFact data, double[] resultCheckedCoef)
+        {
+            double res = 0;
+            for (int i = 0; i < resultCheckedCoef.Count(); i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        res += resultCheckedCoef[i];
+                        break;
+                    case 1:
+                        //a1* item.X2
+                        res += resultCheckedCoef[i] * data.X2;
+                        break;
+                    case 2:
+                        //a2* item.X2* item.X2 +
+                        res += resultCheckedCoef[i] * data.X2 * data.X2;
+                        break;
+                    case 3:
+                        //a3* item.X1 +
+                        res += resultCheckedCoef[i] * data.X1;
+                        break;
+                    case 4:
+                        //   a4 * item.X1 * item.X1 +
+                        res += resultCheckedCoef[i] * data.X1 * data.X1;
+                        break;
+                    case 5:
+                        //a5 * item.X1 * item.X2 +
+                        res += resultCheckedCoef[i] * data.X1 * data.X2;
+                        break;
+                    case 6:
+                        // a6* item.X2* item.X1* item.X1 +
+                        res += resultCheckedCoef[i] * data.X2 * data.X1 * data.X1;
+                        break;
+                    case 7:
+                        //a7* item.X2* item.X2* item.X1 +
+                        res += resultCheckedCoef[i] * data.X2 * data.X2 * data.X1;
+                        break;
+                    case 8:
+                        //a8* item.X1* item.X1* item.X2* item.X2 +
+                        res += resultCheckedCoef[i] * data.X1 * data.X1 * data.X2 * data.X2;
+                        break;
+                    case 9:
+                        //a9* item.X1* item.X1* item.X1 +
+                        res += resultCheckedCoef[i] * data.X1 * data.X1 * data.X1;
+                        break;
+                    case 10:
+                        //a10* item.X2* item.X1* item.X1* item.X1 +
+                        res += resultCheckedCoef[i] * data.X2 * data.X1 * data.X1 * data.X1;
+                        break;
+                    case 11:
+                        //a11* item.X2* item.X2* item.X1* item.X1* item.X1 +
+                        res += resultCheckedCoef[i] * data.X2 * data.X2 * data.X1 * data.X1 * data.X1;
+                        break;
+                    case 12:
+                        //a12* item.X2* item.X2* item.X2 +
+                        res += resultCheckedCoef[i] * data.X2 * data.X2 * data.X2;
+                        break;
+                    case 13:
+                        //a13* item.X2* item.X2* item.X2* item.X1 +
+                        res += resultCheckedCoef[i] * data.X2 * data.X2 * data.X2 * data.X1;
+                        break;
+                    case 14:
+                        //a14* item.X2* item.X2* item.X2* item.X1* item.X1 +
+                        res += resultCheckedCoef[i] * data.X2 * data.X2 * data.X2 * data.X1 * data.X1;
+                        break;
+                    case 15:
+                        //a15* item.X2* item.X2* item.X2* item.X1* item.X1* item.X1);
+                        res += resultCheckedCoef[i] * data.X2 * data.X2 * data.X2 * data.X1 * data.X1 * data.X1;
+                        break;
+                }
+            }
+            return res;
+        }
         private bool CanCalcFromExelCommandExecute(object p)
         {
             return true;
